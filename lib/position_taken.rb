5a3640613516213dd5593719ad116e74a0b4c78b
christian.thomas.2894@gmail.com <christian.thomas.2894@gmail.com>
2018-05-31 23:18:31 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,5 @@
 # code your #position_taken? method here!
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def position_taken?
+  
 end
